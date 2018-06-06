a404741882274fcbea702090340960d067b8950a
tan90 <tan-90@outlook.com>
2018-06-06 12:10:10 -0300

    Add assembler labeled jumps support. Jump instructions can now receive labels as parameters and labeled jumps are converted to load address/jump instruction pair.

--- toolkit/assembler/Assembler.py
+++ toolkit/assembler/Assembler.py
@@ -9,25 +9,77 @@ class Assembler:
     def __init__(self, params, verbose=False):
         self.arg_types = params['arg_types']
         self.instruction_types = params['instruction_types']
+        self.label_instructions = params['label_instructions']
 
         self.instructions = {attribs['alias']: Instruction(attribs) for attribs in params['instructions']}
         
         self.verbose = verbose
 
     def assemble(self, program):
-        lines = program.split('\n')   
-        words = []
+        lines = program.split('\n')
+        lines = Assembler.preprocess_lines(lines)
+
+        lines = self.expand_labels(lines)
+    
+        words = []       
+
+        for line in lines:
+            alias, args = self.parse(line)
+            words.append(self.instructions[alias].assemble(args, self.arg_types, self.instruction_types))
+        return words
+
+    def expand_labels(self, lines):
+        instruction_index = 0
+        processed_lines = []
+        labels = {}
+
+        for line in lines:            
+            if line.startswith('.'):
+                labels[line[1:-1]] = instruction_index
+            else:
+                processed_lines.append(line)
+                separator = line.index(' ')
+                alias = line[:separator]
+
+                if '.' in line and alias in self.label_instructions:
+                    instruction_index += 2
+                else:
+                    instruction_index += 1
+        
+        lines = list(processed_lines)
+        processed_lines = []
+        for line in lines:
+            separator = line.index(' ')
+            alias = line[:separator]
+
+            if '.' in line and alias in self.label_instructions:
+                separator = line.index('.')
+                label = line[separator + 1:]
+                
+                processed_lines.append('li $31, ' + str(labels[label]))
+                processed_lines.append(line[:separator] + '$31')
+            else:
+                processed_lines.append(line)
+
+        return processed_lines
+
+    @staticmethod
+    def preprocess_lines(lines):
+        processed_lines = []
 
         for line in lines:
             if ';' in line:
                 line = line[:line.index(';')]
-                
+
+            while line.startswith(' '):
+                line = line[1:]
+            
             if len(line) == 0:
                 continue
+            else:
+                processed_lines.append(line)
 
-            alias, args = self.parse(line)
-            words.append(self.instructions[alias].assemble(args, self.arg_types, self.instruction_types))
-        return words
+        return processed_lines
     
     def parse(self, line):
         separator = line.index(' ')
@@ -59,6 +111,7 @@ class Assembler:
         return json.loads(json_str)
 
 q = Assembler(Assembler.params_from_file('quanta.json'))
-bits = q.assemble(read_file('program.qtf'))
+# bits = q.assemble(read_file('program.qtf'))
+bits = q.assemble(read_file('C:\\Vinicius\\quanta\\Software\\Toolkit\\Assembler\\out\\artifacts\\Assembler_jar\\Label.qtf'))
 mif = MIF(32, 256, 'BIN', 'BIN', bits)
-write_file('program.mif', mif.as_file())
+write_file('C:\\Vinicius\\quanta\\Hardware\\quanta2\\quanta2inst.mif', mif.as_file())
