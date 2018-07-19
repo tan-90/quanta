3d16ec40755af884751ecd987114ad111271199e
tan90 <tan-90@outlook.com>
2018-07-18 21:54:41 -0300

    Setup css modules.

--- toolkit/wave_generator/src/react/components/Hello.tsx
+++ toolkit/wave_generator/src/react/components/Hello.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import {test} from '../styles/Styles.css'
 
 export interface HelloProps { compiler: string; framework: string; }
 
@@ -6,6 +7,10 @@ export class Hello extends React.Component<HelloProps, {}>
 {
     render()
     {
-        return <h1>Hello from { this.props.compiler } and { this.props.framework }</h1>;
+        return (
+            <h1 className={test}>
+                Hello from {this.props.compiler} and {this.props.framework}
+            </h1>
+        );
     }
 }
--- toolkit/wave_generator/webpack.config.react.js
+++ toolkit/wave_generator/webpack.config.react.js
@@ -19,11 +19,24 @@ module.exports = {
 
     resolve: {
         // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: [".ts", ".tsx", ".js", ".json"]
+        extensions: [".ts", ".tsx", ".js", ".json", ".css"]
     },
 
     module: {
         rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'typings-for-scss-modules-loader',
+                        options: {
+                            modules: true,
+                            namedExport: true
+                        }
+                    }
+                ]
+            },
             // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
             { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
 
